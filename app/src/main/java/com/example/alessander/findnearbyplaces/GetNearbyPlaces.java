2edbea55250be82513f4cd7d3117a65e834940e9
alessanderleite <alessander.logistica@gmail.com>
2018-12-12 07:24:18 -0200

    set googleplaceData

--- app/src/main/java/com/example/alessander/findnearbyplaces/GetNearbyPlaces.java
+++ app/src/main/java/com/example/alessander/findnearbyplaces/GetNearbyPlaces.java
@@ -4,14 +4,27 @@ import android.os.AsyncTask;
 
 import com.google.android.gms.maps.GoogleMap;
 
+import java.io.IOException;
+
 public class GetNearbyPlaces extends AsyncTask<Object, String, String> {
 
-    private String googleplaceDate, url;
+    private String googleplaceData, url;
     private GoogleMap mMap;
 
     @Override
     protected String doInBackground(Object... objects) {
-        return null;
+
+        mMap = (GoogleMap) objects[0];
+        url = (String) objects[1];
+
+        DownloadUrl downloadUrl = new DownloadUrl();
+        try {
+            googleplaceData = downloadUrl.readTheURL(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return googleplaceData;
     }
 
     @Override
